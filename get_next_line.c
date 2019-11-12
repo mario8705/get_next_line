6f1d873ceb731878411d36eb32a808a1659ea852
Alexis Lavaud <alavaud@student.le-101.fr>
2019-11-12 17:56:25 +0100

    get_next_line is now working properly

--- get_next_line.c
+++ get_next_line.c
@@ -6,25 +6,26 @@
 /*   By: alavaud <alavaud@student.le-101.fr>        +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/11/06 17:59:09 by alavaud      #+#   ##    ##    #+#       */
-/*   Updated: 2019/11/08 17:54:17 by alavaud     ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/11/12 17:49:30 by alavaud     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int			ret_val(int n, char *line)
+int			ret_val(int n, int buflen, char **line)
 {
-	int ret;
+	char *linebuf;
 
-	ret = 1;
-	if (n == -1 || !line)
-		ret = -1;
-	if (n == 0 && ft_strlen(line) == 0)
-		ret = 0;
-	if (ret != 1)
-		free(line);
-	return (ret);
+	linebuf = *line;
+	if (!linebuf || n == -1)
+	{
+		free(linebuf);
+		return (-1);
+	}
+	if (buflen == 0)
+		return (0);
+	return (1);
 }
 
 static int	the_get_next_line(int fd, char **line, int lbreak)
@@ -34,7 +35,6 @@ static int	the_get_next_line(int fd, char **line, int lbreak)
 	static int	buflen = 0;
 	int			n;
 
-	*line = NULL;
 	while (!lbreak)
 	{
 		if (bufpos >= buflen)
@@ -48,16 +48,24 @@ static int	the_get_next_line(int fd, char **line, int lbreak)
 		n = bufpos;
 		while (n < buflen && !lbreak)
 			lbreak = (buf[n++] == '\n');
-		if (!str_append(line, buf + bufpos, n - bufpos - lbreak) || buflen == 0)
-			break ;
+		if (!str_append(line, buf + bufpos, n - bufpos - lbreak))
+			return (-1);
 		bufpos = n;
+		if (buflen == 0)
+			break ;
 	}
-	return (ret_val(n, *line));
+	return (ret_val(n, buflen, line));
 }
 
 int			get_next_line(int fd, char **line)
 {
+	int ret;
+
 	if (BUFFER_SIZE <= 0 || fd < 0 || !line)
 		return (-1);
-	return (the_get_next_line(fd, line, 0));
+	*line = NULL;
+	ret = the_get_next_line(fd, line, 0);
+	if (ret == -1)
+		*line = NULL;
+	return (ret);
 }
